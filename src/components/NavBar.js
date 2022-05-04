939cbd46f69be249dd23eee7f3e7494311377284
Catherine Kawara <ndutakawara2@gmail.com>
2022-05-04 11:34:28 +0300

    complete navbar section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const oneLink = links.map(link =>{
+    return <a key={link} href={link}>{link}</a>
+  })
+  return (
+    <nav>
+      {oneLink}
+    </nav>
+  )
 }
 
 export default NavBar;
